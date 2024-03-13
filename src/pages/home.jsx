dd627872a8eb9083201ef8a2166da495e6911213
Gyanendar <gyantiwari10101@gmail.com>
2024-03-13 22:56:13 +0530

    next Previous divs

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/navbar'
 import Footer from '../components/footer'
+import Mainpage from '../components/mainpage'
 const Home = () => {
   return (
     <div>
         <Navbar/>
+        <Mainpage/>
         <Footer/>
     </div>
   )
